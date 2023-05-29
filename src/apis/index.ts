378574cbfbdc8af8ee925ec067bbc43277458e35
yoo-jimin127 <dbwlals9936@naver.com>
2023-05-30 01:38:32 +0900

    feat: 방문자수 API 요청 코드 작성

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,9 +1,33 @@
 import axios from 'axios';
 
-// TODO : baseURL 등록
-const baseURL = `#`;
+const baseURL = `https://yourssu-faster-toilet-api.hyuns.dev`;
 
-export const getResult = async () => {
-  const res = await axios.get(`${baseURL}/buildings`);
-  return res.data;
-}
+export const getBuildingList = async () => {
+  try {
+    const response = await axios.get(`${baseURL}/buildings`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching building list:', error);
+    throw error;
+  }
+};
+
+export const postVisitor = async () => {
+  try {
+    const response = await axios.post(`${baseURL}/visitors`);
+    return response.data;
+  } catch (error) {
+    console.error('Error post visitors: ', error);
+    throw error;
+  }
+};
+
+export const getVisitor = async () => {
+  try {
+    const response = await axios.get(`${baseURL}/visitors`);
+    return response.data;
+  } catch (error) {
+    console.error('Error get visitors: ', error);
+    throw error;
+  }
+};
